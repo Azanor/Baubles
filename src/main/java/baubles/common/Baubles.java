f0be451098adc04d0d09cc7d909515cff158d310
Azanor <lhb@sun.ac.za>
2016-01-01 15:23:33 +0200

    update to MC 1.8.9

--- src/main/java/baubles/common/Baubles.java
+++ src/main/java/baubles/common/Baubles.java
@@ -29,7 +29,7 @@ public class Baubles {
 	
 	public static final String MODID = "Baubles";
 	public static final String MODNAME = "Baubles";
-	public static final String VERSION = "1.1.2.0";
+	public static final String VERSION = "1.1.3.0";
 
 	@SidedProxy(clientSide = "baubles.client.ClientProxy", serverSide = "baubles.common.CommonProxy")
 	public static CommonProxy proxy;
