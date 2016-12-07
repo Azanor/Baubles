796cfea9b711b3cb1f38378a380a307ae6b7818d
Azanor <azanor1@gmail.com>
2016-12-07 13:35:26 +0200

    #179 final tweaks and bugfixes

--- src/main/java/baubles/api/package-info.java
+++ src/main/java/baubles/api/package-info.java
@@ -1,4 +1,4 @@
-@API(owner = "Baubles", apiVersion = "1.3.1.1", provides = "Baubles|API")
+@API(owner = "Baubles", apiVersion = "1.3.1.2", provides = "Baubles|API")
 package baubles.api;
 
 import net.minecraftforge.fml.common.API;
--- src/main/java/baubles/common/Baubles.java
+++ src/main/java/baubles/common/Baubles.java
@@ -33,7 +33,7 @@ public class Baubles {
 	
 	public static final String MODID = "Baubles";
 	public static final String MODNAME = "Baubles";
-	public static final String VERSION = "1.3.6";
+	public static final String VERSION = "1.3.7";
 
 	@SidedProxy(clientSide = "baubles.client.ClientProxy", serverSide = "baubles.common.CommonProxy")
 	public static CommonProxy proxy;
