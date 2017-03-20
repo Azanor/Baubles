945db117acf34b1a692cded9d0f0334e94ec5806
Tobias Wohlfarth <tobias.wohlfarth@googlemail.com>
2017-03-20 04:07:41 +0100

    Fix Javadoc references

--- src/main/java/baubles/api/cap/IBaublesItemHandler.java
+++ src/main/java/baubles/api/cap/IBaublesItemHandler.java
@@ -11,14 +11,12 @@ public interface IBaublesItemHandler extends IItemHandlerModifiable {
 
 	/**
 	 * Used internally to prevent equip/unequip events from triggering when they shouldn't
-	 * @return
 	 */
 	public boolean isEventBlocked();
 	public void setEventBlock(boolean blockEvents);
 
 	/**
 	 * Used internally for syncing. Indicates if the inventory has changed since last sync
-	 * @return
 	 */
 	boolean isChanged(int slot);
 	void setChanged(int slot, boolean changed);
