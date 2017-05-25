6649659550d52cfa400772d26fc3ac5bbfe533ee
Azanor <azanor1@gmail.com>
2017-05-25 08:57:05 +0200

    fixes for localization bug. closes #181

--- src/main/java/baubles/common/Baubles.java
+++ src/main/java/baubles/common/Baubles.java
@@ -32,7 +32,7 @@ public class Baubles {
 	
 	public static final String MODID = "baubles";
 	public static final String MODNAME = "Baubles";
-	public static final String VERSION = "1.4.4";
+	public static final String VERSION = "1.4.5";
 
 	@SidedProxy(clientSide = "baubles.client.ClientProxy", serverSide = "baubles.common.CommonProxy")
 	public static CommonProxy proxy;
