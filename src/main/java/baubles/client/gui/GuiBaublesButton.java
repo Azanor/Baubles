59f3600fa0c6b062dbaed313a726bf4de56ef493
Phylogeny <molecularphylo@gmail.com>
2017-08-19 20:01:47 -0700

    Corrected typo.

--- src/main/java/baubles/client/gui/GuiBaublesButton.java
+++ src/main/java/baubles/client/gui/GuiBaublesButton.java
@@ -17,50 +17,42 @@ public class GuiBaublesButton extends GuiButton {
 		super(buttonId, guiLeft + x, guiTop + y, width, height, buttonText);
 		this.guiLeft = guiLeft;
 	}
-	
-	
-	
+
 	@Override
 	public boolean mousePressed(Minecraft mc, int mouseX, int mouseY) {
-		int potionShift = getPotionShift(mc);
-		return super.mousePressed(mc, mouseX - potionShift, mouseY);
+		return super.mousePressed(mc, mouseX - getPositionShift(mc), mouseY);
 	}
-
-
 
 	@Override
 	public void drawButton(Minecraft mc, int xx, int yy)
-    {
-        if (this.visible)
-        {
-        	int potionShift = getPotionShift(mc);
-        	
-            FontRenderer fontrenderer = mc.fontRenderer;
-            mc.getTextureManager().bindTexture(GuiPlayerExpanded.background);
-            GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-            this.hovered = xx >= this.xPosition + potionShift && yy >= this.yPosition && 
-            		xx < this.xPosition + this.width + potionShift && yy < this.yPosition + this.height;
-            int k = this.getHoverState(this.hovered);
-            GlStateManager.enableBlend();
-            GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
-            GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-            
-            
-            if (k==1) {
-            	this.drawTexturedModalRect(this.xPosition + potionShift, this.yPosition, 200, 48, 10, 10);	
-            } else {
-            	this.drawTexturedModalRect(this.xPosition + potionShift, this.yPosition, 210, 48, 10, 10);
-            	this.drawCenteredString(fontrenderer, I18n.format(this.displayString, new Object[0]), 
-            			this.xPosition + 5 + potionShift, this.yPosition + this.height, 0xffffff);
-            }
-            
-            this.mouseDragged(mc, xx, yy);
-            
-            
-        }
-    }
+	{
+		if (this.visible)
+		{
+			int positionShift = getPositionShift(mc);
 
-	private int getPotionShift(Minecraft mc) {
+			FontRenderer fontrenderer = mc.fontRenderer;
+			mc.getTextureManager().bindTexture(GuiPlayerExpanded.background);
+			GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+			this.hovered = xx >= this.xPosition + positionShift && yy >= this.yPosition &&
+					xx < this.xPosition + this.width + positionShift && yy < this.yPosition + this.height;
+			int k = this.getHoverState(this.hovered);
+			GlStateManager.enableBlend();
+			GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
+			GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+
+			if (k==1) {
+				this.drawTexturedModalRect(this.xPosition + positionShift, this.yPosition, 200, 48, 10, 10);
+			} else {
+				this.drawTexturedModalRect(this.xPosition + positionShift, this.yPosition, 210, 48, 10, 10);
+				this.drawCenteredString(fontrenderer, I18n.format(this.displayString, new Object[0]),
+						this.xPosition + 5 + positionShift, this.yPosition + this.height, 0xffffff);
+			}
+
+			this.mouseDragged(mc, xx, yy);
+		}
+	}
+
+	private int getPositionShift(Minecraft mc) {
 		if (mc.currentScreen instanceof GuiContainer) {
 			GuiContainer guiContainer = (GuiContainer) mc.currentScreen;
 			return guiContainer.getGuiLeft() - this.guiLeft;
