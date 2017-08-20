d0db402abc07ccf5aa0337cb1e8727c9b5e8d720
Phylogeny <molecularphylo@gmail.com>
2017-08-19 21:15:14 -0700

    The inventory baubles button now handles its own logic, rather than doing so in a separate event.

--- src/main/java/baubles/client/gui/GuiBaublesButton.java
+++ src/main/java/baubles/client/gui/GuiBaublesButton.java
@@ -2,61 +2,65 @@ package baubles.client.gui;
 
 import org.lwjgl.opengl.GL11;
 
+import baubles.common.network.PacketHandler;
+import baubles.common.network.PacketOpenBaublesInventory;
+import baubles.common.network.PacketOpenNormalInventory;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.client.gui.inventory.GuiInventory;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.resources.I18n;
 
 public class GuiBaublesButton extends GuiButton {
 
-	private final int guiLeft;
+	private final GuiContainer parentGui;
 
-	public GuiBaublesButton(int buttonId, int guiLeft, int guiTop, int x, int y, int width, int height, String buttonText) {
-		super(buttonId, guiLeft + x, guiTop + y, width, height, buttonText);
-		this.guiLeft = guiLeft;
+	public GuiBaublesButton(int buttonId, GuiContainer parentGui, int x, int y, int width, int height, String buttonText) {
+		super(buttonId, x, parentGui.getGuiTop() + y, width, height, buttonText);
+		this.parentGui = parentGui;
 	}
 
 	@Override
 	public boolean mousePressed(Minecraft mc, int mouseX, int mouseY) {
-		return super.mousePressed(mc, mouseX - getPositionShift(mc), mouseY);
+		boolean pressed = super.mousePressed(mc, mouseX - this.parentGui.getGuiLeft(), mouseY);
+		if (pressed) {
+			if (parentGui instanceof GuiInventory) {
+				PacketHandler.INSTANCE.sendToServer(new PacketOpenBaublesInventory(mc.player));
+			} else {
+				mc.displayGuiScreen(new GuiInventory(mc.player));
+				PacketHandler.INSTANCE.sendToServer(new PacketOpenNormalInventory(mc.player));
+			}
+		}
+		return pressed;
 	}
 
 	@Override
-	public void drawButton(Minecraft mc, int xx, int yy, float p_191745_4_)
+	public void drawButton(Minecraft mc, int mouseX, int mouseY, float partialTicks)
 	{
 		if (this.visible)
 		{
-			int positionShift = getPositionShift(mc);
+			int x = this.x + this.parentGui.getGuiLeft();
 
 			FontRenderer fontrenderer = mc.fontRenderer;
 			mc.getTextureManager().bindTexture(GuiPlayerExpanded.background);
 			GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-			this.hovered = xx >= this.x + positionShift && yy >= this.y &&
-					xx < this.x + this.width + positionShift && yy < this.y + this.height;
+			this.hovered = mouseX >= x && mouseY >= this.y && mouseX < x + this.width && mouseY < this.y + this.height;
 			int k = this.getHoverState(this.hovered);
 			GlStateManager.enableBlend();
 			GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
 			GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 
 			if (k==1) {
-				this.drawTexturedModalRect(this.x + positionShift, this.y, 200, 48, 10, 10);
+				this.drawTexturedModalRect(x, this.y, 200, 48, 10, 10);
 			} else {
-				this.drawTexturedModalRect(this.x + positionShift, this.y, 210, 48, 10, 10);
-				this.drawCenteredString(fontrenderer, I18n.format(this.displayString, new Object[0]),
-						this.x + 5 + positionShift, this.y + this.height, 0xffffff);
+				this.drawTexturedModalRect(x, this.y, 210, 48, 10, 10);
+				this.drawCenteredString(fontrenderer, I18n.format(this.displayString), x + 5, this.y + this.height, 0xffffff);
 			}
 
-			this.mouseDragged(mc, xx, yy);
+			this.mouseDragged(mc, mouseX, mouseY);
 		}
-	}
-
-	private int getPositionShift(Minecraft mc) {
-		if (mc.currentScreen instanceof GuiContainer) {
-			GuiContainer guiContainer = (GuiContainer) mc.currentScreen;
-			return guiContainer.getGuiLeft() - this.guiLeft;
-		}
-		return 0;
 	}
 }
