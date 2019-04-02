9b36407be1e74561126f4be1ba2f8e28a62ba5db
hohserg <hohserg1@yandex.ru>
2019-04-02 21:45:15 +0300

    Correct GlStateManager functions names and some auto-style-correct
    translate->translated
    rotate->rotatef
    and etc

--- src/main/java/baubles/client/BaublesRenderLayer.java
+++ src/main/java/baubles/client/BaublesRenderLayer.java
@@ -2,17 +2,13 @@
  * This class was created by <Vazkii>. It's distributed as
  * part of the Botania Mod. Get the Source Code in github:
  * https://github.com/Vazkii/Botania
- *
+ * <p>
  * Botania is Open Source and distributed under the
  * Botania License: http://botaniamod.net/license.php
- *
+ * <p>
  * File Created @ [Aug 27, 2014, 8:55:00 PM (GMT)]
  */
 package baubles.client;
-
-import javax.annotation.Nonnull;
-
-import org.lwjgl.opengl.GL11;
 
 import baubles.api.BaublesApi;
 import baubles.api.cap.IBaublesItemHandler;
@@ -25,49 +21,52 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.MobEffects;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import org.lwjgl.opengl.GL11;
+
+import javax.annotation.Nonnull;
 
 public final class BaublesRenderLayer implements LayerRenderer<EntityPlayer> {
 
-	@Override
-	public void doRenderLayer(@Nonnull EntityPlayer player, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
-		
-		if(!Config.renderBaubles || player.getActivePotionEffect(MobEffects.INVISIBILITY) != null)
-			return;
+    @Override
+    public void render(@Nonnull EntityPlayer player, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
 
-		IBaublesItemHandler inv = BaublesApi.getBaublesHandler(player);
+        if (!Config.GENERAL.renderBaubles.get() || player.getActivePotionEffect(MobEffects.INVISIBILITY) != null)
+            return;
 
-		dispatchRenders(inv, player, RenderType.BODY, partialTicks);
+        IBaublesItemHandler inv = BaublesApi.getBaublesHandler(player);
 
-		float yaw = player.prevRotationYawHead + (player.rotationYawHead - player.prevRotationYawHead) * partialTicks;
-		float yawOffset = player.prevRenderYawOffset + (player.renderYawOffset - player.prevRenderYawOffset) * partialTicks;
-		float pitch = player.prevRotationPitch + (player.rotationPitch - player.prevRotationPitch) * partialTicks;
+        dispatchRenders(inv, player, RenderType.BODY, partialTicks);
 
-		GlStateManager.pushMatrix();
-		GlStateManager.rotate(yawOffset, 0, -1, 0);
-		GlStateManager.rotate(yaw - 270, 0, 1, 0);
-		GlStateManager.rotate(pitch, 0, 0, 1);
-		dispatchRenders(inv, player, RenderType.HEAD, partialTicks);
-		GlStateManager.popMatrix();
-	}
+        float yaw = player.prevRotationYawHead + (player.rotationYawHead - player.prevRotationYawHead) * partialTicks;
+        float yawOffset = player.prevRenderYawOffset + (player.renderYawOffset - player.prevRenderYawOffset) * partialTicks;
+        float pitch = player.prevRotationPitch + (player.rotationPitch - player.prevRotationPitch) * partialTicks;
 
-	private void dispatchRenders(IBaublesItemHandler inv, EntityPlayer player, RenderType type, float partialTicks) {
-		for(int i = 0; i < inv.getSlots(); i++) {
-			ItemStack stack = inv.getStackInSlot(i);
-			if(stack != null && !stack.isEmpty()) {
-				Item item = stack.getItem();
-				if(item instanceof IRenderBauble) {
-					GlStateManager.pushMatrix();
-					GL11.glColor3ub((byte) 255, (byte) 255, (byte) 255); 
-					GlStateManager.color(1F, 1F, 1F, 1F);
-					((IRenderBauble) stack.getItem()).onPlayerBaubleRender(stack, player, type, partialTicks);
-					GlStateManager.popMatrix();
-				}
-			}
-		}
-	}
+        GlStateManager.pushMatrix();
+        GlStateManager.rotatef(yawOffset, 0, -1, 0);
+        GlStateManager.rotatef(yaw - 270, 0, 1, 0);
+        GlStateManager.rotatef(pitch, 0, 0, 1);
+        dispatchRenders(inv, player, RenderType.HEAD, partialTicks);
+        GlStateManager.popMatrix();
+    }
 
-	@Override
-	public boolean shouldCombineTextures() {
-		return false;
-	}
+    private void dispatchRenders(IBaublesItemHandler inv, EntityPlayer player, RenderType type, float partialTicks) {
+        for (int i = 0; i < inv.getSlots(); i++) {
+            ItemStack stack = inv.getStackInSlot(i);
+            if (stack != null && !stack.isEmpty()) {
+                Item item = stack.getItem();
+                if (item instanceof IRenderBauble) {
+                    GlStateManager.pushMatrix();
+                    GL11.glColor3ub((byte) 255, (byte) 255, (byte) 255);
+                    GlStateManager.color4f(1F, 1F, 1F, 1F);
+                    ((IRenderBauble) stack.getItem()).onPlayerBaubleRender(stack, player, type, partialTicks);
+                    GlStateManager.popMatrix();
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean shouldCombineTextures() {
+        return false;
+    }
 }
