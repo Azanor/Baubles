08e25f34de217a8cbb2d6b363b322361467addaa
hohserg <hohserg1@yandex.ru>
2019-04-02 21:48:57 +0300

    Correct GlStateManager functions and auto-style-correct

--- src/main/java/baubles/client/gui/GuiBaublesButton.java
+++ src/main/java/baubles/client/gui/GuiBaublesButton.java
@@ -1,11 +1,8 @@
 package baubles.client.gui;
 
-import org.lwjgl.opengl.GL11;
-
 import baubles.common.network.PacketHandler;
 import baubles.common.network.PacketOpenBaublesInventory;
 import baubles.common.network.PacketOpenNormalInventory;
-
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiButton;
@@ -13,10 +10,12 @@ import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.client.gui.inventory.GuiInventory;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.resources.I18n;
+import org.lwjgl.opengl.GL11;
 
 public class GuiBaublesButton extends GuiButton {
 
     private final GuiContainer parentGui;
+    private Minecraft mc = Minecraft.getInstance();
 
     public GuiBaublesButton(int buttonId, GuiContainer parentGui, int x, int y, int width, int height, String buttonText) {
         super(buttonId, x, parentGui.getGuiTop() + y, width, height, buttonText);
@@ -38,33 +37,31 @@ public class GuiBaublesButton extends GuiButton {
         return pressed;
     }
 
-	@Override
-	public void drawButton(Minecraft mc, int mouseX, int mouseY, float partialTicks)
-	{
-		if (this.visible)
-		{
-			int x = this.x + this.parentGui.getGuiLeft();
+    @Override
+    public void render(int mouseX, int mouseY, float partialTicks) {
+        if (visible) {
+            int x = this.x + parentGui.getGuiLeft();
 
-			FontRenderer fontrenderer = mc.fontRenderer;
-			mc.getTextureManager().bindTexture(GuiPlayerExpanded.background);
-			GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-			this.hovered = mouseX >= x && mouseY >= this.y && mouseX < x + this.width && mouseY < this.y + this.height;
-			int k = this.getHoverState(this.hovered);
-			GlStateManager.enableBlend();
-			GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
-			GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+            FontRenderer fontrenderer = mc.fontRenderer;
+            mc.getTextureManager().bindTexture(GuiPlayerExpanded.background);
+            GlStateManager.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+            hovered = mouseX >= x && mouseY >= y && mouseX < x + width && mouseY < y + height;
+            int k = getHoverState(hovered);
+            GlStateManager.enableBlend();
+            GlStateManager.blendFuncSeparate(770, 771, 1, 0);
+            GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 
-			GlStateManager.pushMatrix();
-			GlStateManager.translate(0, 0, 200);
-			if (k==1) {
-				this.drawTexturedModalRect(x, this.y, 200, 48, 10, 10);
-			} else {
-				this.drawTexturedModalRect(x, this.y, 210, 48, 10, 10);
-				this.drawCenteredString(fontrenderer, I18n.format(this.displayString), x + 5, this.y + this.height, 0xffffff);
-			}
-			GlStateManager.popMatrix();
+            GlStateManager.pushMatrix();
+            GlStateManager.translatef(0, 0, 200);
+            if (k == 1) {
+                drawTexturedModalRect(x, y, 200, 48, 10, 10);
+            } else {
+                drawTexturedModalRect(x, y, 210, 48, 10, 10);
+                drawCenteredString(fontrenderer, I18n.format(displayString), x + 5, y + height, 0xffffff);
+            }
+            GlStateManager.popMatrix();
 
-			this.mouseDragged(mc, mouseX, mouseY);
-		}
-	}
+            mouseDragged(mouseX, mouseY, 0, 0, 0);
+        }
+    }
 }
